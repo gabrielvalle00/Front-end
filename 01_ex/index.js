53f20888da73be776edd43c8ee2fbb189c9e01e5
BPGTT <grupobpgtt@gmail.com>
2023-09-19 21:45:17 -0300

    JS bruto, atividades

---
+++ 01_ex/index.js
@@ -0,0 +1,7 @@
+//Cria uma variavel para atribuir o nome e o digitalizado pelo o usuário 
+var nome = prompt('Informe o seu nome');
+
+// Cria uma variavrl para armazenar o valor digitado pelo usuário
+var salario = prompt('Qual o valor do seu salário?');
+
+console.log('O funcioário '+nome+' recebe um salário no valor de R$'+salario);
---
+++ 02_ex/index.js
@@ -0,0 +1,10 @@
+//variavel pra amazenar o primeiro numero
+var numb1 = parseInt( prompt ('Digite um valor:'));
+
+//variavel para amazenar o segundo numero
+var numb2 = parseInt( prompt ('Digite um valor :'));
+
+//variavel que vai fazer a soma dos dois numero
+var soma = numb1 + numb2;
+
+console.log('A soma é ' + soma);
